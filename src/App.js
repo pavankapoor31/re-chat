cef93d0b9ca0f7f28369ea24486821b02cdf9d12
Pavan Kapoor <pavankapoor31@gmail.com>
2024-07-17 20:02:07 +0530

    feat: Authentication - login and signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Write from './components/write';
+import Write from './components/write.js';
+import SignIn from './pages/SignIn/SignIn.js';
+import SignUp from './pages/SignUp/SignUp.js';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from './server/firebaseConfig.js';
+import { ToastContainer } from 'react-toastify';
 function App() {
+    const [user] = useAuthState(auth);
   return (
     <div className="">
         <Router>
             <Routes>
-                <Route path="/" element={<Write/>}/>
-                <Route path="/home" element={<Write/>}/>
+                {
+                    !user?  
+                    <>
+                        <Route path="/signup" element={<SignUp/>}/>
+                        <Route path="/signin" element={<SignIn/>}/>
+                    </> 
+                    :
+                    <>
+                        
+                        <Route path="/signup" element={<SignUp/>}/>
+                        <Route path="/signin" element={<SignIn/>}/>
+                        <Route path="/chats:id" element={<Write/>}/>
+                    </>
+                }
             </Routes>
         </Router>
+        <ToastContainer autoClose={2000} />
     </div>
   );
 }
--- src/server/firebaseConfig.js
+++ src/server/firebaseConfig.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,4 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
 export default app;
